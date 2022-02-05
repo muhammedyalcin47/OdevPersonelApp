45c7d770f5416fff7db7d7458f5e1e2eebe00e22
muhammed yalçın <muhammedyalcin4721@gmail.com>
2022-02-05 12:54:42 +0300

    ödevin ilk mikroservisi

---
+++ src/main/java/com/bilgeadam/odevpersonelapp/Exception/PersonelNotFoundException.java
@@ -0,0 +1,11 @@
+package com.bilgeadam.odevpersonelapp.Exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
+public class PersonelNotFoundException extends RuntimeException {
+
+    public PersonelNotFoundException(String message) {
+        super(message);
+    }
+}
